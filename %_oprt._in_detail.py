b881ae7bba37a4ddbf98dbf1d2b0f076e8452ae8
pritammmniet <132140568+pritammmniet@users.noreply.github.com>
2023-05-29 16:19:25 +0530

    Add files via upload

---
+++ %_oprt._in_detail.py
@@ -0,0 +1,19 @@
+#beta version
+#let's have and example to understand this in detail
+'''possible cases are :
+1. a>0 and b>0
+2. a<0 and b>0
+3. a>0 and b<0
+4. a<0 and b<0
+
+the formula used by python
+ans = a - floor(a/b)*b
+
+'''
+print(f"121 % 10 = {121%10} the nrml division value 121/10 = {121/10}")
+print(f"-121 % 10 = {-121%10} the nrml division value -121/10 = {-121/10}")
+print(f"121 % -10 = {121%-10} the nrml division value 121/-10 = {121/-10}")
+print(f"-121 % -10 = {-121%-10} the nrml division value -121/-10 = {-121/-10}")
+
+
+
